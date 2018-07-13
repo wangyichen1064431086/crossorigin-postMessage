662be69b07c8ebddb7b63fca85e44e86d674e2c4
wangyichen1064431086 <1064431086@qq.com>
2018-07-13 14:04:14 +0800

    Basiclly verify the Same Origin Policy

---
+++ server-for-crossorigin-a.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const views = require('koa-views');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(logger());
+
+app.use(views(path.resolve(__dirname,'views'), {
+  map:{
+    html:'nunjucks'
+  }
+}));
+
+async function showPage(pagename, data, ctx) {
+  await ctx.render(pagename,data);
+}
+router.get('/a', showPage.bind(this,'a', {
+  iframeUrl:'http://sub.localhost:3001/b'
+}));
+app.use(router.routes());
+
+app.listen(3000, () => {
+  console.log('Serving crossorigin pages. Listening 3000 with domain localhosts');
+})
